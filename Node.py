9c7ef6bec35613e0709f5732b1a81b473517b117
unknown <g.gad@nu.edu.eg>
2020-07-21 20:28:24 +0200

    architecting

--- Node.py
+++ Node.py
@@ -3,19 +3,28 @@
 from Wallet import Wallet
 from BlockChain import BlockChain 
 
+import os 
+import json
+
 class Node():
-    def __init__(self, network_info_dir):
+    def __init__(self, network_info_dir = None):
         
         self.wallet_dir = None 
         self.chain_dir = None
         self.node_type = 'miner'
-        self.network_info_dir = network_info_dir
-        get_network_info(self.network_info_dir) 
+        self.network_info_path = os.path.join(network_info_dir, 'network_info.json') if network_info_dir else os.path.join(os.getcwd(), 'network_info.json')
+        self.get_network_info(self.network_info_path) 
         
-        self.wallet = Wallet(wallet_dir) 
-        self.blockchain = BlockChain(chain_dir)
+        self.wallet = Wallet(self.wallet_dir) 
+        #self.blockchain = BlockChain(chain_dir)
     
     
-    def get_network_info(self, network_info_dir):
-        
-        pass 
+    def get_network_info(self, network_info_path): 
+        with open(network_info_path, 'r') as file : 
+            info = json.loads(file.read())
+        self.wallet_dir = info['node']['wallet_dir'] if info['node']['wallet_dir'] else os.getcwd() 
+        self.chain_dir = info['node']['chain_dir'] if info['node']['chain_dir'] else os.getcwd() 
+        self.node_type = info['node']['node_type']
+
+
+n = Node() 
--- wallet.py
+++ wallet.py
@@ -10,82 +10,85 @@ import os
 
 class Wallet():
 	
-	def __init__(self, key_dir= None):
-		self.private_Key= None
-		self.public_Key =None
-		self.ser_private_key = None
-		self.ser_public_key = None 
-		self.PADDING = padding.PSS(mgf=padding.MGF1(hashes.SHA256()),  salt_length= padding.PSS.MAX_LENGTH)
-		if key_dir : self.load_key(key_dir)
+    def __init__(self, key_dir= None):
+        self.private_Key= None
+        self.public_Key =None
+        self.ser_private_key = None
+        self.ser_public_key = None 
+        self.PADDING = padding.PSS(mgf=padding.MGF1(hashes.SHA256()),  salt_length= padding.PSS.MAX_LENGTH)
 
+        try: self.load_keys(key_dir)
+        except: 
+            print("wallet couldnot find keys. Generating them... ")
+            self.generate_keys() 
+            self.save_keys()
 
         
-	def generate_keys(self):
-		self.private_key = rsa.generate_private_key(65537,1024,default_backend())
-		self.public_key = self.private_key.public_key() 
+    def generate_keys(self):
+        self.private_key = rsa.generate_private_key(65537,1024,default_backend())
+        self.public_key = self.private_key.public_key() 
 
-	def sign(self, message): 
-		signed = self.private_key.sign(message, self.PADDING, hashes.SHA256())
-		return signed 
+    def sign(self, message): 
+        signed = self.private_key.sign(message, self.PADDING, hashes.SHA256())
+        return signed 
 
-	def verify(self, pub_key, signed, message): 
-		try : 
-			pub_key.verify(signed, message, self.PADDING, hashes.SHA256()) 
-			return True
-		except : return False 
-		
-	def serialize(self):
-		if self.ser_private_key : return self.ser_private_key, self.ser_public_key
+    def verify(self, pub_key, signed, message): 
+        try : 
+            pub_key.verify(signed, message, self.PADDING, hashes.SHA256()) 
+            return True
+        except : return False 
+        
+    def serialize(self):
+        if self.ser_private_key : return self.ser_private_key, self.ser_public_key
 
-		self.ser_private_key = self.private_key.private_bytes(
-			encoding = serialization.Encoding.PEM, 
-			format = serialization.PrivateFormat.PKCS8,
-			encryption_algorithm = serialization.NoEncryption()
-		)
-		self.ser_public_key = self.public_key.public_bytes(
-			encoding = serialization.Encoding.PEM,
-			format = serialization.PublicFormat.SubjectPublicKeyInfo
-		)
-		
-		return self.ser_private_key, self.ser_public_key
+        self.ser_private_key = self.private_key.private_bytes(
+            encoding = serialization.Encoding.PEM, 
+            format = serialization.PrivateFormat.PKCS8,
+            encryption_algorithm = serialization.NoEncryption()
+        )
+        self.ser_public_key = self.public_key.public_bytes(
+            encoding = serialization.Encoding.PEM,
+            format = serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        
+        return self.ser_private_key, self.ser_public_key
 
-	def load_key(self, key_dir = os.getcwd()):
-		public_key_file = os.path.join(key_dir, 'public_key.pem')
-		private_key_file = os.path.join(key_dir, 'private_key.pem')
-		
-		with open(public_key_file, 'rb') as file:
-			self.public_key = serialization.load_pem_public_key(file.read(), backend= default_backend()) 
-		with open(private_key_file, 'rb') as file:
-			self.private_key = serialization.load_pem_private_key(file.read(),password=None, backend= default_backend()) 
-	
-	def save_key(self, key_dir = os.getcwd()):
-		self.serialize() 
+    def load_keys(self, key_dir = os.getcwd()):
+        public_key_file = os.path.join(key_dir, 'public_key.pem')
+        private_key_file = os.path.join(key_dir, 'private_key.pem')
+        
+        with open(public_key_file, 'rb') as file:
+            self.public_key = serialization.load_pem_public_key(file.read(), backend= default_backend()) 
+        with open(private_key_file, 'rb') as file:
+            self.private_key = serialization.load_pem_private_key(file.read(),password=None, backend= default_backend()) 
 
-		public_key_file = os.path.join(key_dir, 'public_key.pem')
-		private_key_file = os.path.join(key_dir, 'private_key.pem')
+    def save_keys(self, key_dir = os.getcwd()):
+        self.serialize() 
 
-		with open(public_key_file, 'wb') as file : file.write(self.ser_public_key) 
-		with open(private_key_file, 'wb') as file : file.write(self.ser_private_key) 
+        public_key_file = os.path.join(key_dir, 'public_key.pem')
+        private_key_file = os.path.join(key_dir, 'private_key.pem')
+
+        with open(public_key_file, 'wb') as file : file.write(self.ser_public_key) 
+        with open(private_key_file, 'wb') as file : file.write(self.ser_private_key) 
 
 
 if __name__ == "__main__":
-	
-	''' generate and save keys
-	w = Wallet() 
-	w.generate_keys() 
 
-	s = b"plain text"
-	s2 = b"plain tex"
+    ''' generate and save keys
+    w = Wallet() 
+    w.generate_keys() 
 
-	signed = w.sign(s)
-	verification_true = w.verify(w.public_key, signed, s)
-	verification_false = w.verify(w.public_key, signed, s2)
+    s = b"plain text"
+    s2 = b"plain tex"
 
-	w.save_key() 
-	'''
-	
-	w2 = Wallet(key_dir = os.getcwd()) 
-	pri, pub = w2.serialize() 
-	print(pri) 
-	print(pub) 
-	
+    signed = w.sign(s)
+    verification_true = w.verify(w.public_key, signed, s)
+    verification_false = w.verify(w.public_key, signed, s2)
+
+    w.save_keys() 
+    '''
+
+    w2 = Wallet(key_dir = os.getcwd()) 
+    pri, pub = w2.serialize() 
+    print(pri) 
+    print(pub) 
